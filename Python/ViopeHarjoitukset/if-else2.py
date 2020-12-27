92cdc7c4e200a03e17daf97cae0506d62c6876d7
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-27 15:18:48 +0200

    if-else valinta 1,2,3,

---
+++ Python/ViopeHarjoitukset/if-else2.py
@@ -0,0 +1,11 @@
+kohde = input("Valitse kohde(1-3):")
+kohde = int(kohde)
+
+if kohde == 1:
+  print("Haukion Kala oy")
+  
+elif kohde == 2:
+    print("Metallipaja VasaraAika")
+
+else:
+  print("Balin palapelitehdas")
