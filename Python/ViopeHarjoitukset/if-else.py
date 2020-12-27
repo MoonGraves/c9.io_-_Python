f2c43ca8fe0e4b375dd9d8f5696dbaba7ef6c8a3
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-27 15:18:14 +0200

    if-else salasana oikein vai väärin

---
+++ Python/ViopeHarjoitukset/if-else.py
@@ -0,0 +1,11 @@
+nimi = input("Anna nimi: ")
+
+if nimi == "Erkki":
+    salasana = input("Anna salasana: ")
+    if salasana == "Esimerkki":
+        print("Salasana ja nimi oli oikein!")
+    else:
+        print("Salasana oli väärin.")
+
+else:
+    print("Nimi oli väärin.") 
