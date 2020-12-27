223d942439f41e53535114f1add74ffc848b03a9
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-27 15:15:30 +0200

    lukee monta kirjainta on tekstissä

---
+++ Python/ViopeHarjoitukset/merkkijono.py
@@ -0,0 +1,8 @@
+muuttuja1 = 10.6411
+muuttuja2 = "Merkkijono"
+
+muuttuja1 = int(muuttuja1)
+muuttuja2 = muuttuja2 *2
+
+print("Kokonaislukumuunnos ei osaa pyöristää:", muuttuja1)
+print("Merkkijonon kertominen tuottaa toistoa:", muuttuja2)
