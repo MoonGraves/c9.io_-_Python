8f9a7d26b67a2935b187df2b9fde54b0f65e61d5
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2021-01-02 14:23:53 +0200

    Python webpage import socketserver

---
+++ Python/Backend_Dev/Webpage/Type04/DefaultUsage.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+#Porttia voidaan käyttää mm. 8080 , 8000 tai 9000
+#Käynnistäessä sitten avaat tyhjän sivun ja sijoitat localhost:8080
+PORT = 8000
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), handler) as httpd:
+    print("Server started at localhost:" + str(PORT))
+    httpd.serve_forever()
